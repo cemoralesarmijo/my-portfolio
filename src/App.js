df9f2b7fec795f72750c1533d7839774dcaaa08b
César Morales Armijo <ce.moralesarmijo@gmail.com>
2022-10-13 00:28:20 -0300

    add header component, add media queries, fix general styles and remove unused react create app packages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
+import React from 'react';
+import "./App.css";
+import Header from "./components/header/Header";
+
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          hello :)
-        </p>
-      </header>
-    </div>
+      <>
+      <Header />
+      </>
   );
 }
 
