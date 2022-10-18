282b621e98b9817180514c5c5198ae48769b57e9
César Morales Armijo <60048138+cemoralesarmijo@users.noreply.github.com>
2022-10-18 12:37:24 -0300

    add home component (#3)

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import "./home.css"
+import Social from "./Social";
+import Data from "./Data";
+import ScrollDown from "./ScrollDown";
+
+const Home = () => {
+    const home_profile = process.env.REACT_APP_HOME_PROFILE_IMAGE
+    return (
+        <section className="home section" id="home">
+            <div className="home__container container grid">
+                <div className="home__content grid">
+                    <Social />
+                    <div  className="home__img"  style={{ background: `url(${home_profile})` }}></div>
+                    <Data />
+                </div>
+                <ScrollDown />
+            </div>
+        </section>
+    );
+};
+
+export default Home;
---
+++ src/components/home/Social.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Social = () => {
+    return (
+        <div className="home__social">
+            <a href="https://github.com/" className="home__social-icon" target="_blank">
+                <i className="uil uil-github"></i>
+            </a>
+            <a href="https://www.linkedin.com/" className="home__social-icon" target="_blank">
+                <i className="uil uil-linkedin"></i>
+            </a>
+            <a href="https://www.instagram.com/" className="home__social-icon" target="_blank">
+                <i className="uil uil-instagram-alt"></i>
+            </a>
+        </div>
+    )
+}
+
+export default Social;
