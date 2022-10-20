b67e764a27c3a42d7e11c13e8914f1c186339469
César Morales Armijo <ce.moralesarmijo@gmail.com>
2022-10-20 02:18:41 -0300

    add scrollup component and fix header menu

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,15 @@
 import React, { useState } from 'react'
 import "./header.css";
 const Header = () => {
+    // change background header
+    window.addEventListener("scroll", function () {
+        const header = document.querySelector(".header");
+        if (this.scrollY >= 560 ) header.classList.add("show-header")
+        else header.classList.remove("show-header")
+    })
+
     const[Toggle, showMenu] = useState(false)
+    const [acticeNav, setActiveNav] = useState("#home");
     return (
         <header className="header">
             <nav className="nav container">
@@ -10,28 +18,42 @@ const Header = () => {
                 <div className={Toggle ? "nav__menu show-manu": "nav__menu"}>
                     <ul className="nav__list grid">
                         <li className="nav__item">
-                            <a href="#home" className="nav__link active-link">
+                            <a href="#home"
+                               onClick={() => setActiveNav('#home')}
+                               className={
+                                acticeNav === "#home"
+                                    ? "nav__link active-link"
+                                    : "nav__link"
+                                }
+                            >
                                 <i className="uil uil-estate nav__icon"></i> Home
                             </a>
                         </li>
                         <li className="nav__item">
-                            <a href="#about" className="nav__link">
+                            <a
+                                href="#about"
+                                onClick={() => setActiveNav('#about')}
+                                className={
+                                    acticeNav === "#about"
+                                        ? "nav__link active-link"
+                                        : "nav__link"
+                            }
+                            >
+
                                 <i className="uil uil-user nav__icon"></i> About
                             </a>
                         </li>
                         <li className="nav__item">
-                            <a href="#skills" className="nav__link">
+                            <a
+                                href="#skills"
+                                onClick={() => setActiveNav('#skills')}
+                                className={
+                                    acticeNav === "#skills"
+                                        ? "nav__link active-link"
+                                        : "nav__link"
+                                }
+                            >
                                 <i className="uil uil-file-alt nav__icon"></i> Skills
-                            </a>
-                        </li>
-                        <li className="nav__item">
-                            <a href="#portfolio" className="nav__link">
-                                <i className="uil uil-scenery nav__icon"></i> Portfolio
-                            </a>
-                        </li>
-                        <li className="nav__item">
-                            <a href="#contact" className="nav__link">
-                                <i className="uil uil-message nav__icon"></i> Contact
                             </a>
                         </li>
                     </ul>
